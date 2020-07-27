c82f863f880f80bc15dad53ee48468dbe85b3924
QuackerDeezles <68830872+QuackerDeezles@users.noreply.github.com>
2020-07-26 17:14:40 -0700

    Update one.js

--- one.js
+++ one.js
@@ -1,3 +1,8 @@
+          // Show the word in black on top of the line
+          ctx.font = "20pt Courier";
+          ctx.fillStyle = "#000000";
+          var xVal = ((answers[i][1] % columnLength) + 1) * 30;
+          var yVal = (parseInt(answers[i][1] / columnLength)+ 1) * 30;
           for (var j = 0; j < answers[i][0].length; j++) {
             var c = answers[i][0].charAt(j);
             ctx.fillText(c, xVal, yVal);
